951cc967081e49f2b36cb2063fbf8dc75f4207b2
Samuel Monsalve Gomez <monsalvegomezsamuel2@gmail.com>
2025-10-07 20:29:01 -0500

    Configuracion de la DB

---
+++ config/database.php
@@ -0,0 +1,19 @@
+<?php
+$host = 'localhost';
+$dbname = 'proyecto_0';
+$user = 'root';
+$pass = 'Samimi2237';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+
+    // Validaciones seguun la respuesta
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Si la conexion es exitosa
+    echo "Conexión exitosa a la base de datos MySQL con PDO.";
+} catch (PDOException $e) {
+    // Si hay algun problema en la base de datos
+    echo "Error al conectar con la base de datos: " . $e->getMessage();
+}
+?>
