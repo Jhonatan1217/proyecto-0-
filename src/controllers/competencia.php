1b2aead272b58689b2ceaec630377f29471fcc1d
Luis <lchernandez4474@gmail.com>
2025-10-08 12:54:33 -0500

    Subiendo cambios de formulario y tablas

--- src/controllers/competencia.php
+++ src/controllers/competencia.php
@@ -1,6 +1,6 @@
 <?php
 
-include("database.php")
+include("database.php");
 
 // INSERTAR
 if (isset($_POST['guardar'])) {
---
+++ src/views/register_tables.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/register_tables.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Proyecto 0 - Visualización de registro de tablas</title>
+    </head>
+    <body class="text-center font-sans">
+
+    <h1 class="text-3xl font-bold mt-6">
+        VISUALIZACIÓN DE REGISTRO TRIMESTRALIZACIÓN - ZONA X
+    </h1>
+    <h2 class="text-xl text-gray-700 mb-6">
+        Sistema de gestión de trimestralización <br> SENA
+    </h2>
+
+    <main class="flex flex-col items-center">
+        <section id="tabla-horarios" class="w-11/12 overflow-x-auto">
+        <table class="border border-gray-700 border-collapse w-full text-sm">
+            <thead>
+            <tr class="bg-green-600 text-white">
+                <th class="border border-gray-700 p-6">Hora</th>
+                <th class="border border-gray-700 p-2">Lunes</th>
+                <th class="border border-gray-700 p-2">Martes</th>
+                <th class="border border-gray-700 p-2">Miércoles</th>
+                <th class="border border-gray-700 p-2">Jueves</th>
+                <th class="border border-gray-700 p-2">Viernes</th>
+                <th class="border border-gray-700 p-2">Sábado</th>
+            </tr>
+            </thead>
+
+            <tbody>
+            <tr class="bg-gray-50 hover:bg-gray-100">
+                <td class="border border-gray-700 p-2 font-medium">6-7</td>
+                <td class="border border-gray-700 p-2">
+                2895664 TGO ADSO <br> J SEBASTIAN DUQUE <br>
+                    Comp: Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae perspiciatis cum nostrum minus ut earum aspernatur doloremque nemo fuga sequi vitae, aliquid quasi quia atque. Ad quia eum recusandae non!
+                </td>
+                <td class="border border-gray-700 p-2">
+                2895664 TGO ADSO <br> J SEBASTIAN DUQUE <br>
+                    Comp: Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae perspiciatis cum nostrum minus ut earum aspernatur doloremque nemo fuga sequi vitae, aliquid quasi quia atque. Ad quia eum recusandae non!
+                </td>
+                <td class="border border-gray-700 p-2">
+                2895664 TGO ADSO <br> J SEBASTIAN DUQUE <br>
+                    Comp: Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae perspiciatis cum nostrum minus ut earum aspernatur doloremque nemo fuga sequi vitae, aliquid quasi quia atque. Ad quia eum recusandae non!
+                </td>
+                <td class="border border-gray-700 p-2">
+                2895664 TGO ADSO <br> J SEBASTIAN DUQUE <br>
+                    Comp: Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae perspiciatis cum nostrum minus ut earum aspernatur doloremque nemo fuga sequi vitae, aliquid quasi quia atque. Ad quia eum recusandae non!
+                </td>
+                <td class="border border-gray-700 p-2">
+                2895664 TGO ADSO <br> J SEBASTIAN DUQUE <br>
+                    Comp: Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae perspiciatis cum nostrum minus ut earum aspernatur doloremque nemo fuga sequi vitae, aliquid quasi quia atque. Ad quia eum recusandae non!
+                </td>
+                <td class="border border-gray-700 p-2">
+                2895664 TGO ADSO <br> J SEBASTIAN DUQUE <br>
+                    Comp: Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae perspiciatis cum nostrum minus ut earum aspernatur doloremque nemo fuga sequi vitae, aliquid quasi quia atque. Ad quia eum recusandae non!
+                </td>
+            </tr>
+
+            <!-- RESTO DE FILAS VACÍAS -->
+            <tr class="bg-white hover:bg-gray-100">
+                <td class="border border-gray-700 p-2">7-8</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+            </tr>
+            <tr class="bg-gray-50 hover:bg-gray-100">
+                <td class="border border-gray-700 p-2">8-9</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+            </tr>
+            <tr class="bg-gray-50 hover:bg-gray-100">
+                <td class="border border-gray-700 p-2">9-10</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+            </tr>
+            <tr class="bg-gray-50 hover:bg-gray-100">
+                <td class="border border-gray-700 p-2">10-11</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+            </tr>
+            <tr class="bg-white hover:bg-gray-100 ">
+                <td class="border border-gray-700 p-2">11-12</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+            </tr>
+            <tr class="bg-white hover:bg-gray-100">
+                <td class="border border-gray-700 p-2">12-13</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+            </tr>
+            <tr class="bg-white hover:bg-gray-100">
+                <td class="border border-gray-700 p-2">13-14</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+            </tr>
+            <tr class="bg-white hover:bg-gray-100">
+                <td class="border border-gray-700 p-2">14-15</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+            </tr>
+            <tr class="bg-white hover:bg-gray-100">
+                <td class="border border-gray-700 p-2">15-16</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+            </tr>
+            <tr class="bg-white hover:bg-gray-100">
+                <td class="border border-gray-700 p-2">16-17</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+            </tr>
+            <tr class="bg-white hover:bg-gray-100">
+                <td class="border border-gray-700 p-2">17-18</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+            </tr>
+            <tr class="bg-white hover:bg-gray-100">
+                <td class="border border-gray-700 p-2">18-19</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+            </tr>
+            <tr class="bg-white hover:bg-gray-100">
+                <td class="border border-gray-700 p-2">19-20</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+            </tr>
+            <tr class="bg-white hover:bg-gray-100">
+                <td class="border border-gray-700 p-2">20-21</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+            </tr>
+            <tr class="bg-white hover:bg-gray-100">
+                <td class="border border-gray-700 p-2">21-22</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+                <td class="border border-gray-700 p-2">&nbsp;</td>
+            </tr>
+            </tbody>
+        </table>
+        </section>
+
+        <div class="mt-6 mb-6 flex gap-6">
+        <button onclick="actualizar()" class="bg-[#00324D] text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition">Actualizar Registro</button>
+        <button onclick="descargarPDF()" class="bg-[#00324D] text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition">Descargar PDF</button>
+        </div>
+    </main>
+
+    <script>
+        function actualizar() {
+        alert("El registro ha sido actualizado correctamente.");
+        }
+        function descargarPDF() {
+        alert("Descargando archivo PDF...");
+        }
+    </script>
+
+</body>
+</html>
