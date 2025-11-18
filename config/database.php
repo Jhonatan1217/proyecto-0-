b27312f0ada42afad5e6135526cc5b2ee94d47b3
kev-dev-v <hh.andress78@gmail.com>
2025-11-18 14:25:36 -0500

    refactor(registerTable.php):se borro el borde de color rojo de el modal de limpiar el horario

--- config/database.php
+++ config/database.php
@@ -1,8 +1,9 @@
 <?php
-$host = 'localhost';
-$dbname = 'proyecto_0';
-$user = 'root';
-$pass = '123456'; 
+// DATOS DE CONEXIÓN EN BYETHOST
+$host   = 'sql213.byethost31.com';           // MySQL Host Name
+$dbname = 'b31_404288824_proyecto_0';        // MySQL DB Name
+$user   = 'b31_404288824';                   // MySQL User Name
+$pass   = 'canelA2006.';    // la misma con la que entras al VistaPanel
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
@@ -10,10 +11,9 @@ try {
 } catch (PDOException $e) {
     header('Content-Type: application/json');
     echo json_encode([
-        "status" => "error",
+        "status"  => "error",
         "mensaje" => "Error al conectar con la base de datos: " . $e->getMessage()
     ]);
     exit;
 }
 ?>
-    
--- src/views/register_tables.php
+++ src/views/register_tables.php
@@ -192,7 +192,7 @@ try {
 
   <!-- Modal Eliminar -->
   <div id="modalEliminar" class="hidden fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-    <div class="bg-white rounded-3xl shadow-2xl p-8 max-w-2xl w-11/12 border-4 border-red-600">
+    <div class="bg-white rounded-3xl shadow-2xl p-8 max-w-2xl w-11/12">
       <div class="flex justify-center mb-4">
         <img class="w-16 h-16" src="<?= BASE_URL ?>src/assets/img/triangle-alert.svg" />
       </div>
